2f66e331f95e203dd96a1d237f8504c6a8dbf614
Fedor Nikolaev <nikolaevfo@gmail.com>
2021-05-27 10:49:21 +0400

    fex cors urls

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-const express = require('express');
-require('dotenv').config();
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const helmet = require('helmet');
-const { errors } = require('celebrate');
-const router = require('./routes');
-const { requestLogger, errorLogger } = require('./middlewares/logger');
-const limiter = require('./limiter');
+const express = require("express");
+require("dotenv").config();
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const helmet = require("helmet");
+const { errors } = require("celebrate");
+const router = require("./routes");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
+const limiter = require("./limiter");
 
-const errorsMiddleware = require('./middlewares/errors');
+const errorsMiddleware = require("./middlewares/errors");
 
-const { PORT = 3000, MONGO_SERVER = 'mongodb://localhost:27017/moviesdb' } = process.env;
+const { PORT = 3000, MONGO_SERVER = "mongodb://localhost:27017/moviesdb" } =
+  process.env;
 
 mongoose.connect(MONGO_SERVER, {
   useNewUrlParser: true,
@@ -25,17 +26,17 @@ app.use(cookieParser());
 
 const corsOptions = {
   origin: [
-    'http://localhost:3000',
-    // 'http://nikolaevfo.movies.nomoredomains.icu',
-    // 'https://nikolaevfo.movies.nomoredomains.icu',
+    "http://localhost:3000",
+    "http://nikolaevfo.movies.nomoredomains.icu",
+    "https://nikolaevfo.movies.nomoredomains.icu",
   ],
-  methods: ['GET', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  methods: ["GET", "PUT", "PATCH", "POST", "DELETE"],
   preflightContinue: false,
   optionsSuccessStatus: 204,
-  allowedHeaders: ['Content-Type', 'origin'],
+  allowedHeaders: ["Content-Type", "origin"],
   credentials: true,
 };
-app.use('*', cors(corsOptions));
+app.use("*", cors(corsOptions));
 
 app.use(requestLogger);
 app.use(limiter);
--- controllers/users.js
+++ controllers/users.js
@@ -1,30 +1,33 @@
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const User = require('../models/user');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 
-const errorsText = require('../utils/constants');
-const NotFoundError = require('../errors/not-found-err');
-const RequestError = require('../errors/req-err');
-const AuthError = require('../errors/auth-err');
-const MongoError = require('../errors/mongo-err');
+const errorsText = require("../utils/constants");
+const NotFoundError = require("../errors/not-found-err");
+const RequestError = require("../errors/req-err");
+const AuthError = require("../errors/auth-err");
+const MongoError = require("../errors/mongo-err");
 
 const getUser = (req, res, next) => {
   const id = req.user._id;
   User.findById(id)
-    .then((user) => {
-      if (!user) {
-        throw new NotFoundError(errorsText.getUser.notFoundError);
+    .then(
+      (user) => {
+        if (!user) {
+          throw new NotFoundError(errorsText.getUser.notFoundError);
+        }
+        res.send(user);
+      },
+      (err) => {
+        if (err.message === "NotValidId") {
+          throw new NotFoundError(errorsText.getUser.notFoundError);
+        } else if (err.name === "CastError") {
+          throw new RequestError(errorsText.getUser.validationError);
+        }
       }
-      res.send(user);
-    }, (err) => {
-      if (err.message === 'NotValidId') {
-        throw new NotFoundError(errorsText.getUser.notFoundError);
-      } else if (err.name === 'CastError') {
-        throw new RequestError(errorsText.getUser.validationError);
-      }
-    })
+    )
     .catch(next);
 };
 
@@ -32,57 +35,65 @@ const updateUser = (req, res, next) => {
   const data = { ...req.body };
   const { email } = data;
   User.findOne({ email })
-    .then((userWithEmail) => {
-      if (userWithEmail && (req.user._id.toString() !== userWithEmail._id.toString())) {
-        throw new MongoError(errorsText.editProfile.alreadyEmailError);
-      }
-      User.findByIdAndUpdate(req.user._id, data, { new: true, runValidators: true })
-        .then((user) => {
-          if (!user) {
-            throw new NotFoundError(errorsText.editProfile.notFoundError);
-          }
-          res.send(user);
+    .then(
+      (userWithEmail) => {
+        if (
+          userWithEmail &&
+          req.user._id.toString() !== userWithEmail._id.toString()
+        ) {
+          throw new MongoError(errorsText.editProfile.alreadyEmailError);
+        }
+        User.findByIdAndUpdate(req.user._id, data, {
+          new: true,
+          runValidators: true,
         })
-        .catch(next);
-    }, (err) => {
-      if (err.message === 'NotValidId') {
-        throw new NotFoundError(errorsText.editProfile.notFoundError);
-      } else if (err.name === 'CastError') {
-        throw new RequestError(errorsText.editProfile.reqError);
-      } else if (err.name === 'ValidationError') {
-        throw new RequestError(errorsText.editProfile.validationError);
+          .then((user) => {
+            if (!user) {
+              throw new NotFoundError(errorsText.editProfile.notFoundError);
+            }
+            res.send(user);
+          })
+          .catch(next);
+      },
+      (err) => {
+        if (err.message === "NotValidId") {
+          throw new NotFoundError(errorsText.editProfile.notFoundError);
+        } else if (err.name === "CastError") {
+          throw new RequestError(errorsText.editProfile.reqError);
+        } else if (err.name === "ValidationError") {
+          throw new RequestError(errorsText.editProfile.validationError);
+        }
       }
-    })
+    )
     .catch(next);
 };
 
 const createUser = (req, res, next) => {
-  const {
-    name, email, password,
-  } = req.body;
+  const { name, email, password } = req.body;
 
   return User.findOne({ email })
     .then((user) => {
       if (user) {
         throw new MongoError(errorsText.signup.emailError);
       } else {
-        bcrypt.hash(password, 10)
-          .then((hash) => {
-            User.create({
-              name, email, password: hash,
+        bcrypt.hash(password, 10).then((hash) => {
+          User.create({
+            name,
+            email,
+            password: hash,
+          })
+            .then((newUser) => {
+              if (!newUser) {
+                throw new RequestError(errorsText.signup.reqError);
+              }
+              const sendUser = {
+                name: newUser.name,
+                email: newUser.email,
+              };
+              res.status(201).send({ user: sendUser });
             })
-              .then((newUser) => {
-                if (!newUser) {
-                  throw new RequestError(errorsText.signup.reqError);
-                }
-                const sendUser = {
-                  name: newUser.name,
-                  email: newUser.email,
-                };
-                res.status(201).send({ user: sendUser });
-              })
-              .catch(next);
-          });
+            .catch(next);
+        });
       }
     })
     .catch(next);
@@ -91,7 +102,8 @@ const createUser = (req, res, next) => {
 const login = (req, res, next) => {
   const { email, password } = req.body;
 
-  return User.findOne({ email }).select('+password')
+  return User.findOne({ email })
+    .select("+password")
     .then((user) => {
       if (!user) {
         throw new AuthError(errorsText.signin.emailPasswordError);
@@ -104,21 +116,22 @@ const login = (req, res, next) => {
           }
           const token = jwt.sign(
             { _id: user._id },
-            NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
-            { expiresIn: '7d' },
+            NODE_ENV === "production" ? JWT_SECRET : "dev-secret",
+            { expiresIn: "7d" }
           );
           const sendUser = {
             name: user.name,
             email: user.email,
           };
           res
-            .cookie('jwt', token, {
+            .cookie("jwt", token, {
               maxAge: 3600000,
               httpOnly: true,
-              sameSite: 'None',
-              secure: NODE_ENV === 'production',
+              sameSite: "None",
+              secure: NODE_ENV === "production",
             })
-            .status(200).send({ user: sendUser });
+            .status(200)
+            .send({ user: sendUser });
         })
         .catch(next);
     })
@@ -128,11 +141,13 @@ const login = (req, res, next) => {
 const signout = (req, res, next) => {
   Promise.resolve(true)
     .then(() => {
-      res.clearCookie('jwt', {
-        httpOnly: true,
-        sameSite: 'None',
-        secure: true,
-      }).send({ message: errorsText.signout.clearCookie });
+      res
+        .clearCookie("jwt", {
+          httpOnly: true,
+          sameSite: "None",
+          secure: true,
+        })
+        .send({ message: errorsText.signout.clearCookie });
     })
     .catch(next);
 };
